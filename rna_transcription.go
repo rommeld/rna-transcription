0ae92ff88d43a65a203679553d5fa7029f3d9de7
rommeld <rommel.denny@gmail.com>
2020-12-01 17:24:57 +0100

    Update the function. Tests go though acuratly.

--- rna_transcription.go
+++ rna_transcription.go
@@ -1,4 +1,20 @@
+/*
+Package strand changes DNA nucleotides for RNA.
+*/
 package strand
 
-func ToRNA(dna string) string {
+//ToRNA receives a DNA strand and returns its RNA complement.
+func ToRNA(dna string) (solution string) {
+	for _, nuc := range dna {
+		if nuc == 'G' {
+			solution += "C"
+		} else if nuc == 'C' {
+			solution += "G"
+		} else if nuc == 'T' {
+			solution += "A"
+		} else if nuc == 'A' {
+			solution += "U"
+		}
+	}
+	return solution
 }
