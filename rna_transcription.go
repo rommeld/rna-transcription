8ac144ba533ce030c4821f26dd1933f6240cd026
rommeld <rommel.denny@gmail.com>
2020-12-01 17:46:57 +0100

    I used another way to come to the same conclusion.

--- rna_transcription.go
+++ rna_transcription.go
@@ -3,18 +3,37 @@ Package strand changes DNA nucleotides for RNA.
 */
 package strand
 
+import "bytes"
+
+var transcript = map[rune]string{
+	'G': "C",
+	'C': "G",
+	'T': "A",
+	'A': "U",
+}
+
 //ToRNA receives a DNA strand and returns its RNA complement.
 func ToRNA(dna string) (solution string) {
+	var buf bytes.Buffer
+
+	/*
+		When working with a if conditionals you do not need to define a transcript
+		or buf. Therefore bytes has not to be imported.
+			for _, nuc := range dna {
+				if nuc == 'G' {
+					solution += "C"
+				} else if nuc == 'C' {
+					solution += "G"
+				} else if nuc == 'T' {
+					solution += "A"
+				} else if nuc == 'A' {
+					solution += "U"
+				}
+			}
+	*/
+
 	for _, nuc := range dna {
-		if nuc == 'G' {
-			solution += "C"
-		} else if nuc == 'C' {
-			solution += "G"
-		} else if nuc == 'T' {
-			solution += "A"
-		} else if nuc == 'A' {
-			solution += "U"
-		}
+		buf.WriteString(transcript[nuc])
 	}
-	return solution
+	return buf.String()
 }
